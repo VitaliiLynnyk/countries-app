3a0285a5dbacdc1dc4d4103a0476feb5ba0e5d47
Vitalii Lynnyk <vitaliilynnyk@Vitaliis-MacBook-Air.local>
2019-09-04 17:51:52 +0300

    Updates

---
+++ precache-manifest.efc0438c72ac055ba54ba85ff20c77e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0357f6833f82cd58a6ed02a74c4b2f90",
+    "url": "/countries-app/index.html"
+  },
+  {
+    "revision": "516c2625d5fdc7908502",
+    "url": "/countries-app/static/css/2.c0851dc3.chunk.css"
+  },
+  {
+    "revision": "6ec99bb2a19bf8b48b60",
+    "url": "/countries-app/static/css/main.765cb144.chunk.css"
+  },
+  {
+    "revision": "516c2625d5fdc7908502",
+    "url": "/countries-app/static/js/2.e9443d50.chunk.js"
+  },
+  {
+    "revision": "6ec99bb2a19bf8b48b60",
+    "url": "/countries-app/static/js/main.53eaa5bd.chunk.js"
+  },
+  {
+    "revision": "6628de3e423c345dd422",
+    "url": "/countries-app/static/js/runtime~main.e95b6172.js"
+  }
+]);
